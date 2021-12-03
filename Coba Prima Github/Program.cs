7b0fbe2b007811f79ac2a36337d211d9fc11f395
Jemmie Renard <jemmierenard17@gmail.com>
2021-12-03 10:33:41 +0700

    Membuat coding cek prima

--- Coba Prima Github/Program.cs
+++ Coba Prima Github/Program.cs
@@ -10,7 +10,29 @@ namespace Coba_Prima_Github
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Masukkan angka untuk cek prima : ");
+            int angka = Convert.ToInt32(Console.ReadLine());
+            int jumlah = 0;
+            for (int i = 1; i <= angka; i++)
+            {
+                int sisa = angka % i;
+                if (sisa == 0)
+                {
+                    jumlah = jumlah + 1;
+                }
+                else
+                {
+                    jumlah = jumlah;
+                }
+            }
+            if (jumlah > 2)
+            {
+                Console.WriteLine("Bukan anka prima");
+            }
+            else
+            {
+                Console.WriteLine("Angka prima");
+            }
         }
     }
 }
